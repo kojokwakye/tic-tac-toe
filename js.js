e9947535b0464020877cb1cccd9d8e0b773e1e4e
kojokwakye <kojokwakye808@gmail.com>
2025-07-04 23:42:44 +0000

    skeleton files

--- js.js
+++ js.js
@@ -1,1 +1,29 @@
-console.log("we good?");
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  // create a 2d array.
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+
+   const players = [
+    {
+      name: playerOneName,
+      token: X
+    },
+    {
+      name: playerTwoName,
+      token: O
+    }
+  ];
+  // This will be the method of getting the entire board that our
+  // UI will eventually need to render it.
+  const getBoard = () => board;
+}
+
