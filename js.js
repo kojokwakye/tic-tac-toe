0f086d12d2d1b995b4558e71e8676688c4e87e62
kojokwakye <kojokwakye808@gmail.com>
2025-07-11 17:21:49 +0000

    can print board

--- js.js
+++ js.js
@@ -1,42 +1,67 @@
 function Gameboard() {
+  function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+      value = player;
+    };
+    const getValue = () => value;
+
+    return { addToken, getValue };
+  }
+
   const rows = 3;
   const columns = 3;
   const board = [];
 
-  // 2d array
+  // 2d board
+
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
       board[i].push(Cell());
     }
   }
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
 
-  function Cell() {
-    let value = 0;
-  }
+  return { printBoard };
+}
 
-  function GameController(
-    playeroneName = "player one",
-    playertwoName = "player two"
-  ) {
-    const board = Gameboard();
+function GameController(
+  playerOneName = "player one",
+  playerTwoName = "player two"
+) {
+  const board = Gameboard();
 
-    const players = [
-      {
-        name: playeroneName,
-        token: X,
-      },
-      {
-        name: playertwoName,
-        token: O,
-      },
-    ];
-    let activePlayer = players[0];
+  const players = [
+    {
+      name: playerOneName,
+      token: "X",
+    },
+    {
+      name: playerTwoName,
+      token: "0",
+    },
+  ];
+  let activePlayer = players[0];
 
-    const switchPlayerTurn = () => {
-      activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-  }
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const playRound = (column) => {
+    console.log(`dfdsf ${column}`);
+    board(column);
+    switchPlayerTurn();
+  };
+
+  return { playRound, printBoard: board.printBoard };
 }
 
 const game = GameController();
+game.printBoard();
