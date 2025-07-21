527d80ad3133a4498f5552480e4cb2b8c22035ec
kojokwakye <kojokwakye808@gmail.com>
2025-07-21 22:18:31 +0000

    revert

--- js.js
+++ js.js
@@ -127,4 +127,14 @@ game.playRound(1, 0);
 game.playRound(2, 0);
 game.playRound(2, 2);
 game.playRound(2, 1);
+// play round to declare win for X
+game.playRound(0, 0);
+game.playRound(0, 1);
+game.playRound(1, 1);
+game.playRound(0, 2);
+game.playRound(1, 2);
+game.playRound(1, 0);
+game.playRound(2, 0);
+game.playRound(2, 2);
+game.playRound(2, 1);
 game.printBoard();
