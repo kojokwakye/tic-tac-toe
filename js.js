f250ed9862c038192f53ddbc9779babea524b8b1
kojokwakye <kojokwakye808@gmail.com>
2025-07-30 05:04:58 +0000

    board shows

--- js.js
+++ js.js
@@ -1,35 +1,27 @@
 "use strict";
-const container = document.getElementById("container");
 
 function Gameboard() {
-  function Cell() {
-    let value = 0;
-    const addToken = (player) => {
-      value = player;
-    };
-    const getValue = () => value;
-
-    return { addToken, getValue };
-  }
-
-  //  2d array
   const rows = 3;
   const columns = 3;
   const board = [];
-
-  const bits = document.createDocumentFragment();
 
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
       board[i].push(Cell());
-      const div = document.createElement("div");
-      bits.appendChild(div).classList.add("squares");
     }
   }
-  container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-  container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-  container.appendChild(bits);
+
+  const getBoard = () => board;
+
+  const tokenPlacement = (column, row, player) => {
+    if (board[row][column].getValue() === 0) {
+      board[row][column].addToken(player);
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   // printing board
   const printBoard = () => {
@@ -39,24 +31,23 @@ function Gameboard() {
     );
     console.log(boardWithCellValues);
   };
-
-  // dropping tokens
-  const getBoard = () => board;
-  const tokenPlacement = (column, row, token) => {
-    if (board[row][column].getValue() === 0) {
-      // chec if the cell is empty
-      board[row][column].addToken(token); // places the token if empty
-      return true;
-    } else {
-      return false;
-    }
-  };
   return { getBoard, tokenPlacement, printBoard };
+}
+
+function Cell() {
+  let value = 0;
+
+  const addToken = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return { addToken, getValue };
 }
 
 function GameController() {
   const board = Gameboard();
-
   // player variables
   const players = [
     {
@@ -68,48 +59,18 @@ function GameController() {
       token: "O",
     },
   ];
+
   let activePlayer = players[0];
 
   // switch player
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
-
   const getActivePlayer = () => activePlayer;
 
   const printNewRound = () => {
     board.printBoard();
     console.log(`${getActivePlayer().token}'s turn`);
-  };
-
-  const playRound = (column, row) => {
-    const moveSucessful = board.tokenPlacement(
-      column,
-      row,
-      getActivePlayer().token
-    );
-    console.log(
-      `${getActivePlayer().name} dropped ${
-        getActivePlayer().token
-      } ${column},${row} `
-    );
-
-    if (moveSucessful) {
-      const winner = checkWin();
-      if (winner) {
-        board.printBoard();
-        setTimeout(() => console.log(`${winner.name || winner} wins!`), 2000);
-        return;
-      } else if (fullBoard()) {
-        return;
-      }
-      // switch player
-      switchPlayerTurn();
-      printNewRound();
-    } else {
-      console.log("spot taken");
-      board.printBoard();
-    }
   };
 
   const winLogic = [
@@ -169,9 +130,10 @@ function GameController() {
         values[0] !== 0
       ) {
         const winner = players.find((player) => player.token === values[0]);
-        return winner; // returns the whole player object
+        return winner;
       }
     }
+
     return false; // no win
   };
 
@@ -186,22 +148,89 @@ function GameController() {
         }
       }
     }
+    console.log("tie");
     board.printBoard();
-    setTimeout(() => console.log("tie"), 2000);
     return true;
   };
 
+  const playRound = (column, row) => {
+    const moveSucessful = board.tokenPlacement(
+      column,
+      row,
+      getActivePlayer().token
+    );
+
+    console.log(
+      `${getActivePlayer().name} dropped ${
+        getActivePlayer().token
+      } (${column},${row}) `
+    );
+    board.tokenPlacement(column, row, getActivePlayer().token);
+
+    if (moveSucessful) {
+      const winner = checkWin();
+      if (winner) {
+        setTimeout(() => console.log(`${winner.name || winner} wins!`), 2000);
+        board.printBoard();
+        return;
+      } else if (fullBoard()) {
+        return true;
+      }
+      switchPlayerTurn();
+      printNewRound();
+    } else {
+      console.log("spot taken");
+      board.printBoard();
+    }
+  };
+
   printNewRound();
-  return { playRound, checkWin, fullBoard };
+
+  return {
+    playRound,
+    getActivePlayer,
+    checkWin,
+    fullBoard,
+    getBoard: board.getBoard,
+  };
 }
 
-const game = GameController();
-// test game by copying any of the logics from /PROJECTS/tic-tac-toe/tests.js
-// game.playRound(0, 1);
-// game.playRound(0, 2);
-// game.playRound(1, 1);
-// game.playRound(2, 1);
-// game.playRound(1, 2);
-// game.playRound(0, 0);
-// game.playRound(1, 0);
-//
+function controller() {
+  const game = GameController();
+
+  const playerTurnDiv = document.querySelector(".turn");
+  const container = document.getElementById("container");
+
+  const updatescreen = () => {
+    // clear the board
+    container.textContent = "";
+
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // display player turn
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+    board.forEach((row) => {
+      row.forEach((cell, index) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function
+        cellButton.dataset.column = index;
+        cellButton.textContent = cell.getValue();
+        container.appendChild(cellButton);
+      });
+    });
+  };
+  function clickHandlerBoard(e) {
+  
+
+    game.playRound();
+    updatescreen();
+  }
+  container.addEventListener("click", clickHandlerBoard);
+  updatescreen();
+}
+
+controller();
